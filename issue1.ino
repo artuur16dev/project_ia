045980512a82a1c3a46425f8de4e477f3d13315d
artuur16dev <arturoventura132@gmail.com>
2026-02-19 13:26:49 +0100

    Add files via upload
    closes #1

---
+++ issue1.ino
@@ -0,0 +1,14 @@
+const int LED_PIN = 25;
+
+void setup() {
+
+  Serial.begin(115200);
+  delay(200);
+
+  Serial.println("Sistema llest! Escriu ON o OFF i prem Enter.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
